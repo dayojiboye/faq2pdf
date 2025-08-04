48e3df5301a02b3d3356a1ed6bd8b20c99aca35e
dayojiboye <my@personal>
2025-08-04 09:44:18 +0100

    feat: Updated footer and prompt for AI

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,16 +3,62 @@ import { Button } from "./ui/button";
 import Link from "next/link";
 
 export default function Footer() {
-	return (
-		<footer className="mt-auto w-full">
-			<div className="container mx-auto px-4 py-3">
-				<p className="inline text-sm">Made by</p>{" "}
-				<Button asChild variant="link" size={"text"} className="whitespace-nowrap">
-					<Link href="https://github.com/dayojiboye" target="_blank" referrerPolicy="no-referrer">
-						Dee 👑
-					</Link>
-				</Button>
-			</div>
-		</footer>
-	);
+  return (
+    <footer className="mt-auto w-full">
+      <div className="container mx-auto px-4 py-3 flex flex-wrap gap-2 items-center justify-between">
+        <div className="w-fit">
+          <p className="inline text-sm">
+            Made by{" "}
+            <Button
+              asChild
+              variant="link"
+              size={"text"}
+              className="whitespace-nowrap"
+            >
+              <Link
+                href="https://github.com/dayojiboye"
+                target="_blank"
+                referrerPolicy="no-referrer"
+              >
+                Dee 👑
+              </Link>
+            </Button>
+          </p>
+        </div>
+
+        <p className="w-fit text-sm">
+          Powered by{" "}
+          <Button
+            asChild
+            variant="link"
+            size={"text"}
+            className="whitespace-nowrap"
+          >
+            <Link
+              href="https://ai.google.dev/"
+              target="_blank"
+              referrerPolicy="no-referrer"
+            >
+              Gemini
+            </Link>
+          </Button>{" "}
+          &{" "}
+          <Button
+            asChild
+            variant="link"
+            size={"text"}
+            className="whitespace-nowrap"
+          >
+            <Link
+              href="https://pptr.dev/"
+              target="_blank"
+              referrerPolicy="no-referrer"
+            >
+              Puppeteer
+            </Link>
+          </Button>
+        </p>
+      </div>
+    </footer>
+  );
 }
